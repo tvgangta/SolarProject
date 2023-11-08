33eadf8d4e840a37e74b99eeca6a03fcf066a5bd
tvgangta <tushargangta56@gmail.com>
2023-11-08 18:32:45 +0530

    Maven Project for SPRINGBOOT added

---
+++ BackEnd/solarproject/src/main/java/com/solar/solarproject/SolarprojectApplication.java
@@ -0,0 +1,13 @@
+package com.solar.solarproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarprojectApplication.class, args);
+	}
+
+}
---
+++ BackEnd/solarproject/src/test/java/com/solar/solarproject/SolarprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solar.solarproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
