6bb2d948009e257582817a33a6ad2fa1d3cca440
tvgangta <tushargangta56@gmail.com>
2023-11-08 17:29:39 +0530

    REACT Project Created

---
+++ FrontEnd/REACT/react-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+function App() {
+  return (
+    <>
+    <Navbar title="What's up maaaaahNerds!"/>
+    </>
+  );
+}
+
+export default App;
---
+++ FrontEnd/REACT/react-app/src/Components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Navbar(props) {
+  return (
+    <h1>{props.title}</h1>
+  )
+}
